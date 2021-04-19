d64ef449ffbdc320ee3d2f76a6798f14499dabf2
Rafael Pardinas <39837166+rafapi@users.noreply.github.com>
2021-04-19 18:21:55 +0100

    Create paragraphs.py

---
+++ paragraphs.py
@@ -0,0 +1,25 @@
+import re
+import textract
+import numpy as np
+
+from pandas import DaraFrame
+
+
+text = '200309-sustainable-finance-teg-final-report-taxonomy-annexes_en.pdf'
+
+# Extract raw text from PDF file
+raw_text = textract.process(text, method='pdfminer')
+
+# Split by paragraph and remove blank spaces
+text_split = re.split('\n\n', raw_text.decode())
+stripped = [x.strip() for x in text_split]
+
+# Store paragraphs in a Pandas DataFrame
+df = DataFrame(stripped, columns=['paragraph'])
+
+# Convert the emptly strings to NaN so that we can drop them
+df['paragraph'].replace('', np.nan, inplace=True)
+df.dropna(subset=['paragraph'], inplace=True)
+
+# Save to csv
+df.to_csv('paragrapsh.csv')
